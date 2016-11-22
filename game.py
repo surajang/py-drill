f7bcbbbf615473da7445608ab80cd76180a7a6cf
surajang <memswap@gmail.com>
2016-11-22 16:12:14 +0900

    initial code upload. This is my personal python practice. ;)

---
+++ game.py
@@ -0,0 +1,10 @@
+
+# -*- coding: UTF-8 -*-
+print "##############################"
+print "#      WTF RPG WORLD         #"
+print "# somewhat like retro game.. #"
+print "# 한글도 나올 수 있나요?     #"
+print "##############################"
+print ""
+var = raw_input("What's your name, hero? : ")
+print "Ah, nice to meet you,",var,"."
