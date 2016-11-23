50dbb909ff18ecd19be127575c38e4207dca0fca
surajang <memswap@gmail.com>
2016-11-23 18:01:25 +0900

    Separate hero code

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 import fibo #test for module import
 import random
+import hero
 
 def intro():
     """print title text-thingy stuff on start."""
@@ -11,33 +12,11 @@ def intro():
     print "##############################"
     print ""
 
-class hero:
-    """Manage player's status info."""
-    userName = ""
-    locationV = "N1"
-    locationH = "W3"
-    hpMax = 100
-    mpMax = 50
-    currentHP = 100
-    currentMP = 50
-
-    def __init__(self, inputName):
-        """Initiate user profile with name"""
-        self.userName = inputName
-
-    def stat(self):
-        """Display player's status"""
-        print "현재위치 "+ self.locationV + "-" + self.locationH + " | HP " + str(self.currentHP) + "/" + str(self.hpMax) + " | " + str(self.currentMP) + "/" + str(self.mpMax)
-
-    def getUserName(self):
-        """Returns player's name"""
-        return self.userName
-
 intro()
 
 print "[안내원] 환영합니다, 낯선 여행자님. 당신은 누구신가요?"
-var = raw_input("이름을 입력하세요: ")
-user = hero(var) #create player profile
+userInputName = raw_input("이름을 입력하세요: ")
+user = hero.hero(userInputName) #create player profile
 print "[안내원] Ah, nice to meet you,",user.getUserName() + "."
 
 user.stat() # display user status
