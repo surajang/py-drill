c062824bc06208fda080245ba8e8d71bd22766b2
surajang <memswap@gmail.com>
2016-11-23 18:01:04 +0900

    Another module import test

---
+++ hero.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+class hero:
+    """Manage player's status info."""
+    userName = ""
+    locationV = "N1"
+    locationH = "W3"
+    hpMax = 100
+    mpMax = 50
+    currentHP = 100
+    currentMP = 50
+
+    def __init__(self, inputName):
+        """Initiate user profile with name"""
+        self.userName = inputName
+
+    def stat(self):
+        """Display player's status"""
+        print "현재위치 "+ self.locationV + "-" + self.locationH + " | HP " + str(self.currentHP) + "/" + str(self.hpMax) + " | " + str(self.currentMP) + "/" + str(self.mpMax)
+
+    def getUserName(self):
+        """Returns player's name"""
+        return self.userName
